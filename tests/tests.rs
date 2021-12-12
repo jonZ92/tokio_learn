916d76decd9fbf54ec133a73ec51edd2ab4db1f4
jon <zjon234@gmail.com>
2021-12-12 18:11:19 +0800

    测试案例

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,3 +1,5 @@
+
+
 #[cfg(test)]
 
 mod tests {
@@ -9,15 +11,11 @@ mod tests {
             Some(43) => println!("ok"),
             _ => (),
         }
-
         if let Some(43) = a {
             println!(" is  ok");
         }
-
         let x: i32 = 2;
-
         let b: Option<i32> = Some(4);
-
         let c = x + b.unwrap();
         println!("{}", c);
     }
@@ -25,7 +23,6 @@ mod tests {
     #[test]
     fn test2() {
         let version = [1, 2, 3, 4];
-
         let t: Result<i32, i32> = if version[0] == 1 { Ok(1) } else { Err(2) };
         println!("{:?}", t);
         // match version[4] {
@@ -35,6 +32,6 @@ mod tests {
         // 如果   t 是Err  执行大括号里的代码
         if let Err(e) = t {
             println!("错了 错了");
-        }
+        };
     }
 }
