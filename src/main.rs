811f37844b6cb77d0da8cc70a30003d78b6acc95
jon <zjon234@gmail.com>
2021-12-12 16:10:59 +0800

    添加文档

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,50 @@
-
 /**************************************************************************************
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  **************************************************************************************/
 
-use std::thread::sleep;
+// use std::thread::sleep;
 
-use tokio::time::Duration;
+// use tokio::time::Duration;
 
+use std::fmt::Result;
 
+use tokio::net::TcpListener;
+
+use tokio::io::{AsyncReadExt,AsyncWriteExt};
+
+/*
+#[tokio::main]
+async fn main() {
+    // tokio::task::spawn_blocking(||{
+    //     sleep(Duration::from_millis(10000));
+    //     println!("hello ");
+
+    // }).await.unwrap();
+
+    // println!("world");
+
+    // tokio::task::spawn(async {
+    //     sleep(Duration::from_millis(5000));
+    //     println!("hello");
+    // });
+
+    // println!("world");
+
+}
+*/
 
 
 #[tokio::main]
-async fn main() {
-    
-    tokio::task::spawn_blocking(||{
-        sleep(Duration::from_millis(10000));
-        println!("hello ");
+async fn main()->Result<(),Box<dyn std::error::Error>>{
 
-    }).await.unwrap();
 
-    println!("world");
+    Ok(())
 }
