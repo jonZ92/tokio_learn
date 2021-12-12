056e08672be6e57b21d35eff113bec25dcd7277f
jon <zjon234@gmail.com>
2021-12-12 16:04:39 +0800

    添加文档

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,19 @@
 
 use std::thread::sleep;
 
+use tokio::time::Duration;
 
 
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    
+    tokio::task::spawn_blocking(||{
+        sleep(Duration::from_millis(10000));
+        println!("hello ");
+
+    }).await.unwrap();
+
+    println!("world");
 }
