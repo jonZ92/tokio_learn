e3578b5b0331a001a43b3a259ae455f2895e1153
jon <zjon234@gmail.com>
2021-12-12 18:00:22 +0800

    测试案例

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,9 @@
 
 // use tokio::time::Duration;
 
-use std::fmt::Result;
-
 use tokio::net::TcpListener;
 
-use tokio::io::{AsyncReadExt,AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 /*
 #[tokio::main]
@@ -41,10 +39,32 @@ async fn main() {
 }
 */
 
-
 #[tokio::main]
-async fn main()->Result<(),Box<dyn std::error::Error>>{
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("hello world");
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
 
+            loop {
+                let n = match socket.read(&mut buf).await {
+                 
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
 
-    Ok(())
+            
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
 }
---
+++ tests/tests.rs
@@ -0,0 +1,40 @@
+#[cfg(test)]
+
+mod tests {
+
+    #[test]
+    fn test1() {
+        let a = Some(43);
+        match a {
+            Some(43) => println!("ok"),
+            _ => (),
+        }
+
+        if let Some(43) = a {
+            println!(" is  ok");
+        }
+
+        let x: i32 = 2;
+
+        let b: Option<i32> = Some(4);
+
+        let c = x + b.unwrap();
+        println!("{}", c);
+    }
+
+    #[test]
+    fn test2() {
+        let version = [1, 2, 3, 4];
+
+        let t: Result<i32, i32> = if version[0] == 1 { Ok(1) } else { Err(2) };
+        println!("{:?}", t);
+        // match version[4] {
+        //     Ok(v)=> println!("working with version: {:?}", v),
+        //     Err(e)=> println!("error parsing header: {:?}", e),
+        // }
+        // 如果   t 是Err  执行大括号里的代码
+        if let Err(e) = t {
+            println!("错了 错了");
+        }
+    }
+}
