206f5b91a5353c339107dfab8c6349737c3fc4b6
jon <zjon234@gmail.com>
2021-12-13 22:32:58 +0800

    第一步 测试

--- src/main.rs
+++ src/main.rs
@@ -42,25 +42,26 @@ async fn main() {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
-    println!("hello world");
+   
     loop {
         let (mut socket, _) = listener.accept().await?;
         tokio::spawn(async move {
             let mut buf = [0; 1024];
-
+            println!("hello world");
             loop {
                 let n = match socket.read(&mut buf).await {
                  
-                    Ok(n) if n == 0 => return,
+                    Ok(n) if n == 0 => {println!("n is = {}",n); return},
                     Ok(n) => n,
                     Err(e) => {
                         eprintln!("failed to read from socket; err = {:?}", e);
                         return;
                     }
+                    
                 };
-
-            
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                println!("{:?}",&buf[0..n]);
+                let ok="OK".as_bytes();
+                if let Err(e) = socket.write_all(&ok).await {
                     eprintln!("failed to write to socket; err = {:?}", e);
                     return;
                 }
